054f97caea9157bb38881728ab3be02ba5260f03
5yntaxError404 <63261805+5yntaxError404@users.noreply.github.com>
2023-11-01 11:57:49 -0400

    updated changes

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,62 +1,67 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+
 import '../css/LoginPage.css';
 
-const Login = (props) => {
+function Login() {
 
-    const [username,setUserName] = useState()
-    const [password, setPassword] = useState()
+    var username,password;
+    const [message,setMessage] = useState('');
 
-	const doLogin = async() => {
-        axios.post('http://localhost:5000/api/login', {username,password})
-        .then(result => console.log(result))
-        .catch(err=> console.log(err));
+	const doLogin = async event => {
+
+		var obj = {
+			"UserName": username.value,
+			"Password": password.value
+		};
+		var js = JSON.stringify(obj);
+	
+		try {
+			const response = await fetch('http://localhost:4000/api/login', {
+				method: 'post',
+				body: js,
+				headers: { 'Content-Type': 'application/json' }
+			});
+			var res = JSON.parse(await response.text());
+			console.log(res);
+
+			 if (res.error) {
+                setMessage('Unable to Login'); // Set an error message
+                console.log("Some error");
+            } 
+            else {
+                setMessage('Logged In.'); // Set a success message
+                console.log("Logged In");
+            }
+		} catch (e) {
+			alert(e.toString());
+			return;
+		}
     }
-
-    useEffect(() => {
-        doLogin();
-    },[])
-        
-		/*try
-		{
-            const response = await fetch('http://localhost:3000/api/login', {
-            method: "POST",
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-			},
-            body: JSON.stringify(obj),
-        })
-		*/
-
-
 
     return (
 	<div className="login-container">
         <div className="login-form">
-            <form className='form'>
+            <form className='form' onSubmit={doLogin}>
             <h3>Sign In</h3>
 
             <div className="mb-3">
             <label>Username</label>
-            <input
-                type="text"
-				id="loginUserName"
-                className="form-control"
-                placeholder="Enter username"
-                name="username"
-                onChange={(e) => setUserName(e.target.value)}
-            />
+            <input 
+						type="text" 
+						id="username" 
+						className="user-input-field" 
+						placeholder="Username" 
+						ref={(c) => username = c}/><br />
             </div>
 
             <div className="mb-3">
             <label>Password</label>
-            <input
-				id="loginPassword"
-                className="form-control"
-                placeholder="Enter password"
-                name="password"
-                onChange={(e) => setPassword(e.target.value)}
-            />
+            <input 
+						type="password" 
+						id="password" 
+						className="user-input-field" 
+						placeholder="Password" 
+						ref={(c) => password = c}/><br />
             </div>
 
             <div className="mb-3">
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,9 +1,6 @@
-import React, { useEffect,useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom'
-import '../css/LoginPage.css';
+import React, { useState } from 'react';
 
-import { useNavigate } from 'react-router-dom';
+import '../css/LoginPage.css';
 
 function SignUp(){
 
