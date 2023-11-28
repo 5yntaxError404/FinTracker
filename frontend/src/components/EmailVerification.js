4a6a5679bf26a41061f3918973c260b09b71c738
5yntaxError404 <63261805+5yntaxError404@users.noreply.github.com>
2023-11-28 08:29:09 -0500

    added redirect to login, and changed CSS text.

--- frontend/src/components/EmailVerification.js
+++ frontend/src/components/EmailVerification.js
@@ -1,47 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import '../css/LandingPage.css';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
-// Looking for correct code to import font
-
 const EmailVerification = () => {
+  const history = useHistory();
+  const [isVerified, setIsVerified] = useState(false);
 
-    useEffect(() => {
-      const verifyEmail = async () => {
-        const verificationToken = new URLSearchParams(window.location.search).get('token');
-  
-        if (verificationToken) {
-          try {
-            const response = await fetch(`https://www.fintech.davidumanzor.com/verify-email?token=${verificationToken}`, {
+  useEffect(() => {
+    const verifyEmail = async () => {
+      const verificationToken = new URLSearchParams(window.location.search).get('token');
 
-				method: 'post',
-				headers: { 'Content-Type': 'application/json' }
-			});
-          } catch (error) {
-            console.error(error);
-            alert('Email verification failed. verification token: ' + verificationToken);
+      if (verificationToken) {
+        try {
+          const response = await fetch(`https://www.fintech.davidumanzor.com/verify-email?token=${verificationToken}`, {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' }
+          });
+
+          // Assuming your server responds with a success status when verification is successful
+          if (response.status === 200) {
+            setIsVerified(true);
+          } else {
+            setIsVerified(false);
           }
-        } else {
-          alert('Invalid verification link.');
+        } catch (error) {
+          console.error(error);
+          alert('Email verification failed. Verification token: ' + verificationToken);
         }
-      };
-  
-      verifyEmail();
-    }, []); // Run the effect only once when the component mounts
+      } else {
+        alert('Invalid verification link.');
+      }
+    };
 
-    return (
+    verifyEmail();
+  }, []); // Run the effect only once when the component mounts
 
-        <div className="landing-container">
-            <div className="content">
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono"></link>
-                <p>Verifying...</p>
-               
-            </div>
-        </div>
-    );
-}
+  // Redirect to login after verification
+  useEffect(() => {
+    if (isVerified) {
+      // Redirect to the login page
+      history.push('/Login');
+    }
+  }, [isVerified, history]);
 
-
+  return (
+    <div className="landing-container">
+      <div className="content">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono"></link>
+        {isVerified ? <p>Verified! Redirecting to login...</p> : <p>Verifying...</p>}
+      </div>
+    </div>
+  );
+};
 
 export default EmailVerification;
