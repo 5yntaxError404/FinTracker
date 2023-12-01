a23fcbebeb5071d150594837bb2392e17426a982
5yntaxError404 <63261805+5yntaxError404@users.noreply.github.com>
2023-11-30 20:40:35 -0500

    RIP

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -15,7 +15,15 @@ function Login() {
 
   // Function to parse JWT token
   function parseJwt(token) {
-    // ... (your existing parseJwt function)
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+        .join('')
+    );
+    return JSON.parse(jsonPayload);
   }
 
   const errorPush = (message, fields) => {
